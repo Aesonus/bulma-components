d8b1488e7799847904ac76e201c6dcd5ee69d9d4
Cory Laughlin <corylcomposinger@gmail.com>
2022-05-14 11:10:19 -0500

    Updated public api

--- projects/bulma-components-library/src/public-api.ts
+++ projects/bulma-components-library/src/public-api.ts
@@ -2,4 +2,14 @@
  * Public API Surface of bulma-components-library
  */
 
+export * from './lib/navbar/navbar/navbar.component'
+export * from './lib/navbar/navbar-brand/navbar-brand.component'
+export * from './lib/navbar/navbar-divider/navbar-divider.component'
+export * from './lib/navbar/navbar-dropdown/navbar-dropdown.component'
+export * from './lib/navbar/navbar-end/navbar-end.component'
+export * from './lib/navbar/navbar-item/navbar-item.component'
+export * from './lib/navbar/navbar-start/navbar-start.component'
 export * from './lib/navbar/navbar.module'
+
+export * from './lib/hero/hero/hero.component'
+export * from './lib/hero/hero.module'
